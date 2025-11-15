7656b8cd206453211ecebd145bcee1a969b0a53f
Idandot <fokinmita589@gmail.com>
2025-11-15 16:16:44 +0300

    BC 0.1.3.2 HexRigidbody

--- Base Class/Actor.gd
+++ Base Class/Actor.gd
@@ -1,14 +1,11 @@
 extends Node
 class_name Actor
 
-var _axial_position: Vector2i
-var _facing: HexOrientation = HexOrientation.new()
-
 #добавить больше сигналов по необходимости
 signal setup_started(config: ActorConfig)
 signal turn_ended(actor: Actor)
 signal turn_started(actor: Actor)
-signal facing_changed(_facing: HexOrientation)
+
 
 @export var modules: Array[PackedScene] = []
 
@@ -16,12 +13,7 @@ var _initiative = 0
 var _is_alive = true
 
 func setup(config: ActorConfig):
-	setup_started.emit(config.duplicate())
-	if config.spawn_point != null:
-		set_position(config.spawn_point)
-	if config.initial_facing != null:
-		set_facing(config.initial_facing)
-
+	setup_started.emit(config)
 
 func take_turn():
 	emit_signal("turn_started", self)
@@ -39,14 +31,7 @@ func get_initiative():
 func is_alive():
 	return _is_alive
 
-func set_position(axial: Vector2i):
-	_axial_position = axial
-	_axial_position = AxialUtilities.axial_clamp(_axial_position, HexGridClass.get_grid_radius())
-	self.position = AxialUtilities.axial_to_world(_axial_position)
 
-func set_facing(value):
-	_facing.set_direction(value)
-	facing_changed.emit(_facing)
 
 
 
---
+++ Base Class/HexRigidbody.gd
@@ -0,0 +1,38 @@
+extends Node
+class_name HexRigidbody
+
+var _facing = HexOrientation.new()
+var _axial_position = Vector2i.ZERO
+var _velocity = Vector2i.ZERO
+var _velocities: Array[Vector2i] = []
+var _acceleration = Vector2i.ZERO
+var _accelerations: Array[Vector2i] = []
+
+@onready var parent: Node2D = self.get_parent()
+@onready var event_bus: Node
+#You stopped here
+
+signal facing_changed(_facing: HexOrientation)
+
+func set_position(axial: Vector2i):
+	_axial_position = axial
+	_axial_position = AxialUtilities.axial_clamp(_axial_position, HexGridClass.get_grid_radius())
+	self.position = AxialUtilities.axial_to_world(_axial_position)
+
+func set_facing(value):
+	_facing.set_direction(value)
+	facing_changed.emit(_facing)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
